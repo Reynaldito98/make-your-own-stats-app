04d0e1821609efebb430d6cf2112ef75f5778e4c
Reynaldo Perez Pauli <reynaldoperezpauli6@gmail.com>
2025-01-18 20:21:06 -0600

    Update project

--- frontend/baseball-app/src/utils/api/news.api.js
+++ frontend/baseball-app/src/utils/api/news.api.js
@@ -1,4 +1,4 @@
-const apiKey = '183def0c951d4c2b9f084dbf18b3df1c';
+const apiKey = 'BlzFiz0ALsWFAnkHTr9t2wUCR0Hmvgl0TieQopbK';
 const currentDate = new Date();
 const sevenDaysDate = new Date(currentDate. getTime() - 7 * 24 * 60 * 60 * 1000);
 const today = `${currentDate.getFullYear()}-${String(currentDate.getMonth()+1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
@@ -7,7 +7,7 @@ const dateSevenDaysAgo = `${sevenDaysDate.getFullYear()}-${String(sevenDaysDate.
 import {checkResponse} from './utils.js';
 
 const getNews = (teamName) => {
-    return fetch(`https://api.thenewsapi.com/v1/news/all?locale=us&language=en&api_token=BlzFiz0ALsWFAnkHTr9t2wUCR0Hmvgl0TieQopbK&search=${teamName}&country=us$published_after=${dateSevenDaysAgo}&published_before=${today}`)
+    return fetch(`https://api.thenewsapi.com/v1/news/all?locale=us&language=en&api_token=${apiKey}&search=${teamName}&country=us$published_on=${today}`)
         .then(checkResponse)
 }
 
